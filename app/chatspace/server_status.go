a356c90222fb2feab07903e7b031b101efc604a1
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2022-11-21 06:51:49 +0000

    #1: Add message and End end processing

--- app/chatspace/server_status.go
+++ app/chatspace/server_status.go
@@ -15,8 +15,8 @@ const (
 	serverStatusModeWork
 )
 
-var (
-	timeStep  = time.Minute
+const (
+	timeStep  = time.Second
 	chatTimes = 15 * timeStep
 	workTimes = 45 * timeStep
 )
@@ -41,6 +41,7 @@ func NewServerStatus(baseLogger *zap.Logger, sess *discordgo.Session, scheduler 
 	if err != nil {
 		return nil, err
 	}
+
 	ss := &ServerStatus{
 		logger: baseLogger.With(
 			zap.String("feature", "serverStatus"),
@@ -54,6 +55,13 @@ func NewServerStatus(baseLogger *zap.Logger, sess *discordgo.Session, scheduler 
 		scheduler:         scheduler,
 		memberIDs:         make(map[string]struct{}),
 		managedChannelIDs: make(map[string]struct{}),
+	}
+
+	if msg, err := ss.sess.ChannelMessageSendEmbed(ss.channelID, &discordgo.MessageEmbed{
+		Title:       "💕よろしくおねがいします！",
+		Description: "この度は来てくださりありがとうございます。しっかり作業部屋を運営してまいりますのでよろしくお願いします。",
+	}); err != nil {
+		ss.logger.Error("failed send message", zap.String("channelID", msg.ChannelID), zap.Error(err))
 	}
 
 	ss.Switch2Chat()
@@ -70,6 +78,7 @@ func (ss *ServerStatus) onVoiceChangeUpdate(sess *discordgo.Session, event *disc
 	ss.lock.Lock()
 	defer ss.lock.Unlock()
 
+	isClose = false
 	userId := event.Member.User.ID
 	if event.BeforeUpdate == nil {
 		event.BeforeUpdate = &discordgo.VoiceState{}
@@ -101,6 +110,10 @@ func (ss *ServerStatus) onVoiceChangeUpdate(sess *discordgo.Session, event *disc
 		if _, exist := ss.memberIDs[userId]; exist {
 			ss.logger.Debug("Left from chatspace", zap.String("userID", userId))
 			delete(ss.memberIDs, userId)
+
+			if len(ss.memberIDs) == 0 {
+				isClose = true
+			}
 		}
 	}
 
@@ -109,12 +122,16 @@ func (ss *ServerStatus) onVoiceChangeUpdate(sess *discordgo.Session, event *disc
 			ss.logger.Error("cannot change mute", zap.String("userID", userId), zap.String("changeTo", "unmute"), zap.Error(err))
 		}
 	}
-	return false
+	return isClose
 }
 
 func (ss *ServerStatus) Switch2Work() {
 	ss.lock.Lock()
 	defer ss.lock.Unlock()
+
+	if ss.isClosed {
+		return
+	}
 
 	ss.logger.Info("switch mode chat to work")
 	ss.mode = serverStatusModeWork
@@ -122,6 +139,16 @@ func (ss *ServerStatus) Switch2Work() {
 		if err := ss.sess.GuildMemberMute(ss.guildID, memberId, true); err != nil {
 			ss.logger.Error("cannot change mute", zap.String("userID", memberId), zap.String("changeTo", "mute"), zap.Error(err))
 		}
+	}
+
+	if msg, err := ss.sess.ChannelMessageSendEmbed(ss.channelID, &discordgo.MessageEmbed{
+		Title:       "🚀作業時間です！",
+		Description: "作業は45分間です。作業中はミュートを行うので次の休憩までに作業を進めましょう。",
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "休憩時間は" + time.Now().Add(9*time.Hour).Format(time.Kitchen) + "ごろからです",
+		},
+	}); err != nil {
+		ss.logger.Error("failed send message", zap.String("channelID", msg.ChannelID), zap.Error(err))
 	}
 
 	ss.scheduler.PushBack(ScheduleEvent{
@@ -134,12 +161,26 @@ func (ss *ServerStatus) Switch2Chat() {
 	ss.lock.Lock()
 	defer ss.lock.Unlock()
 
+	if ss.isClosed {
+		return
+	}
+
 	ss.logger.Info("switch mode work to chat")
 	ss.mode = serverStatusModeChat
 	for memberId := range ss.memberIDs {
 		if err := ss.sess.GuildMemberMute(ss.guildID, memberId, false); err != nil {
 			ss.logger.Error("cannot change mute", zap.String("userID", memberId), zap.String("changeTo", "unmute"), zap.Error(err))
 		}
+	}
+
+	if msg, err := ss.sess.ChannelMessageSendEmbed(ss.channelID, &discordgo.MessageEmbed{
+		Title:       "🌿休憩時間です！",
+		Description: "休憩は15分間です。休憩中はミュートを外すので好きに話してください。",
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "作業時間は" + time.Now().Add(9*time.Hour).Format(time.Kitchen) + "ごろからです",
+		},
+	}); err != nil {
+		ss.logger.Error("failed send message", zap.String("channelID", msg.ChannelID), zap.Error(err))
 	}
 
 	ss.scheduler.PushBack(ScheduleEvent{
@@ -150,5 +191,12 @@ func (ss *ServerStatus) Switch2Chat() {
 
 func (ss *ServerStatus) Close() error {
 	ss.isClosed = true
+
+	if msg, err := ss.sess.ChannelMessageSendEmbed(ss.channelID, &discordgo.MessageEmbed{
+		Title:       "🤗またお越しください！",
+		Description: "私はすぐに駆け付けます。ボイスチャンネルにまた来てください。",
+	}); err != nil {
+		ss.logger.Error("failed send message", zap.String("channelID", msg.ChannelID), zap.Error(err))
+	}
 	return ss.voiceConn.Disconnect()
 }
