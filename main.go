a98c0c464532ac83758809cdc27f199df75572c9
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2022-11-21 06:05:09 +0000

    #1:  chatspace を稼働できるように修正

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/streamwest-1629/chatspace/app/chatspace"
 	"go.uber.org/zap"
 )
 
@@ -17,8 +19,19 @@ func init() {
 }
 
 func main() {
+
+	// healthcheck
 	logger.Debug("initialize application")
 	http.HandleFunc("/_hck", healthcheck)
+
+	// chatspace application
+	discordToken := os.Getenv("CHATSPACE_DISCORD_TOKEN")
+	controller, err := chatspace.NewService(logger, discordToken)
+	if err != nil {
+		logger.Error("cannot start chatspace application", zap.String("discordToken", discordToken[:8]+"***"+discordToken[len(discordToken)-8:]), zap.Error(err))
+	}
+	defer controller.Close()
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logger.Fatal("listen server is ended", zap.Error(err))
 	}
