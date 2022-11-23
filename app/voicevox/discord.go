6aee2943d4265005c38123d97cc5ee1a42dda65e
Kasai Kou <streamwest1629@gmail.com>
2022-11-23 04:08:34 +0000

    #21: dgvoice代替を適用

--- app/voicevox/discord.go
+++ app/voicevox/discord.go
@@ -5,15 +5,11 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"sync"
 
-	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
-	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"layeh.com/gopus"
 )
@@ -60,9 +56,8 @@ func (m *ManagedDiscordVoiceConnection) GetSpeakers(nameFilter string, waitResum
 }
 
 type DiscordVoiceConnection struct {
-	genQueueQuit   chan<- *sync.WaitGroup
-	speakQueueQuit chan<- *sync.WaitGroup
-	generateQueue  chan<- generateVoiceArgs
+	genQueueQuit  chan<- *sync.WaitGroup
+	generateQueue chan<- generateVoiceArgs
 }
 
 type generateVoiceArgs struct {
@@ -74,15 +69,15 @@ type generateVoiceArgs struct {
 func StartDiscordVoiceConnection(appLogger *zap.Logger, vc *discordgo.VoiceConnection, voiceVox *VoiceVox, replaceFn func(input string) string) *DiscordVoiceConnection {
 
 	type speakQueueArgs struct {
-		filename string
-		wg       *sync.WaitGroup
+		binary  io.ReadCloser
+		process Killer
+		wg      *sync.WaitGroup
 	}
 
 	var (
-		genQueueQuit   = make(chan *sync.WaitGroup)
-		generateQueue  = make(chan generateVoiceArgs)
-		speakQueueQuit = make(chan *sync.WaitGroup)
-		speakQueue     = make(chan speakQueueArgs)
+		genQueueQuit  = make(chan *sync.WaitGroup)
+		generateQueue = make(chan generateVoiceArgs)
+		speakQueue    = make(chan speakQueueArgs)
 	)
 
 	go func(queue chan<- speakQueueArgs) {
@@ -93,82 +88,44 @@ func StartDiscordVoiceConnection(appLogger *zap.Logger, vc *discordgo.VoiceConne
 				return
 
 			case args := <-generateQueue:
-				sended := func() (sended bool) {
-					if replaceFn != nil {
-						args.content = replaceFn(args.content)
-					}
+				func() {
+					defer func() {
+						// finalize operation for call event ended
+						if args.wg != nil {
+							args.wg.Done()
+						}
+					}()
 
 					appLogger.Debug("voicevox generate request recieved", zap.String("content", args.content))
 
-					tempDir := filepath.Join(filepath.Dir(os.Args[0]), "./.tmp/")
 					wav, err := voiceVox.GenerateVoice(args.content, args.speakerID, false)
 					if err != nil {
 						appLogger.Error("failed to generate voice", zap.Int("speakerId", args.speakerID), zap.Error(err))
-						return false
+						return
 					}
-					defer wav.Close()
 
-					if err := os.MkdirAll(tempDir, os.ModePerm); err != nil {
-						if err != os.ErrExist {
-							appLogger.Error("failed to mkdir", zap.Error(err))
-							return false
+					go func() {
+						defer wav.Close()
+						ffmpegout, process, err := ffmpegConvert(wav)
+						if err != nil {
+							appLogger.Error("convert error by ffmpeg", zap.Error(err))
+							return
 						}
-					}
+						defer ffmpegout.Close()
+						defer process.Kill()
 
-					fileName := filepath.Join(tempDir, uuid.NewString()+".wav")
-					file, err := os.Create(fileName)
-					if err != nil {
-						appLogger.Error("failed to create wave file", zap.String("filename", fileName), zap.Error(err))
-						return false
-					}
-					defer file.Close()
-
-					if _, err := io.Copy(file, wav); err != nil {
-						appLogger.Error("failed to save wave file", zap.String("filename", fileName), zap.Error(err))
-						return false
-					}
-
-					speakQueue <- speakQueueArgs{
-						filename: fileName,
-						wg:       args.wg,
-					}
-
-					return true
+						if err := playAudio(appLogger, vc, ffmpegout, process); err != nil {
+							appLogger.Error("cannot play audio", zap.Error(err))
+						}
+					}()
 				}()
-				if !sended {
-					if args.wg != nil {
-						args.wg.Done()
-					}
-				}
-			}
-		}
-	}(speakQueue)
-
-	go func(queue <-chan speakQueueArgs) {
-		for {
-			select {
-			case wg := <-speakQueueQuit:
-				defer wg.Done()
-				select {
-				case args := <-queue:
-					os.Remove(args.filename)
-				default:
-					return
-				}
-			case args := <-queue:
-				dgvoice.PlayAudioFile(vc, args.filename, make(chan bool))
-				os.Remove(args.filename)
-				if args.wg != nil {
-					args.wg.Done()
-				}
 			}
 		}
 	}(speakQueue)
 
 	return &DiscordVoiceConnection{
-		genQueueQuit:   genQueueQuit,
-		speakQueueQuit: speakQueueQuit,
-		generateQueue:  generateQueue,
+		genQueueQuit:  genQueueQuit,
+		generateQueue: generateQueue,
 	}
 }
 
@@ -178,9 +135,6 @@ func (d *DiscordVoiceConnection) Quit() {
 		wg := sync.WaitGroup{}
 		wg.Add(1)
 		d.genQueueQuit <- &wg
-		wg.Wait()
-		wg.Add(1)
-		d.speakQueueQuit <- &wg
 		wg.Wait()
 	}
 }
